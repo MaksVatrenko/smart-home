923306a0889c9c75def8e78d66b71bc2c2c8cc78
maksVatrenko <maks1004200097@gmail.com>
2026-04-08 15:46:56 +0300

    swiper

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -46,14 +46,44 @@
       </div>
     </div>
     <div class="page__content">
-      <Icon name="i:community"></Icon>
-      <div class="page__slider"></div>
+      <Icon class="page__icon" name="i:community"></Icon>
+      <div class="page__swiper-container swiper">
+        <button type="button" @click="slidePrev">
+          <Icon
+            class="swiper__icon"
+            name="material-symbols:arrow-back-ios-rounded"
+          ></Icon>
+        </button>
+        <Swiper
+          :navigation="true"
+          :loop="true"
+          :space-between="50"
+          @swiper="onSwiper"
+        >
+          <SwiperSlide v-for="slide in slides" :key="slide.id">
+            <NuxtLink class="swiper__item" :to="slide.routerLink">
+              <NuxtImg class="swiper__img" :src="slide.img" alt="Slide Image" />
+              <span class="swiper__title">
+                {{ t(slide.title) }}
+              </span>
+            </NuxtLink>
+          </SwiperSlide>
+        </Swiper>
+        <button type="button" @click="slideNext">
+          <Icon
+            class="swiper__icon"
+            name="material-symbols:arrow-forward-ios-rounded"
+          ></Icon>
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
 
 const { t } = useI18n()
 
@@ -71,6 +101,13 @@ type Weather = {
     | 'sun'
 }
 
+type Slide = {
+  id: number
+  img: string
+  routerLink: string
+  title: string
+}
+
 const name = 'Alina'
 const avatar =
   'https://instagram.fkiv9-1.fna.fbcdn.net/v/t51.82787-15/619334815_18074341892615625_5174968488304016953_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=110&ig_cache_key=MjcwNTMyNzEzMjc2NDA2Njk2Mw%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE0NDB4MTgwMC5zZHIuQzMifQ%3D%3D&_nc_ohc=xsLSGmFKEQUQ7kNvwFp8EcO&_nc_oc=AdqusctNg7_lyyD0wCt0_DGSBkJ55uPM2_ssbm7Nd-3hfaV0g2vECJTiY2jDs37z97s&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fkiv9-1.fna&_nc_gid=BRsMc8QR1kTf-d1XUooMHA&_nc_ss=7a32e&oh=00_Af1g_dN8ME6SFfp3Nv9HV-agJLSROCOBtTRDe0bH_2huaw&oe=69D57F39'
@@ -78,6 +115,38 @@ const weather: Weather = {
   tempOutside: 28,
   tempIndoor: 18,
   condition: 'sun-behind-cloud'
+}
+const slides = ref<Slide[]>([
+  {
+    id: 1,
+    img: 'https://evolveartisanal.com/wp-content/uploads/2021/07/1_The-Wooden-Rhapsody-Modern-Bedroom-Interior-Design.jpg',
+    routerLink: '/bedroom',
+    title: 'bedroom'
+  },
+  {
+    id: 2,
+    img: 'https://hips.hearstapps.com/hmg-prod/images/1737-q54a-jm-0403-lowres-designer-jennifer-mcgee-67dc655a1d1ac.jpg?crop=0.8886666666666666xw:1xh;center,top&resize=1200:*',
+    routerLink: '/bathroom',
+    title: 'bathroom'
+  }
+])
+
+const swiperInstance = ref<any>(null)
+
+const onSwiper = (swiper: any) => {
+  swiperInstance.value = swiper
+}
+
+function slidePrev() {
+  if (swiperInstance.value) {
+    swiperInstance.value.slidePrev()
+  }
+}
+
+function slideNext() {
+  if (swiperInstance.value) {
+    swiperInstance.value.slideNext()
+  }
 }
 </script>
 
@@ -144,12 +213,28 @@ const weather: Weather = {
     height: 100%;
     object-fit: cover;
   }
+
   &__weather {
     margin-top: em(30);
     display: flex;
     align-items: center;
     justify-content: center;
     gap: em(40);
+  }
+
+  &__icon {
+    width: em(24);
+    height: em(24);
+    margin-bottom: em(16);
+  }
+
+  &__content {
+    margin-top: em(28);
+  }
+
+  &__swiper-container {
+    display: flex;
+    gap: em(10);
   }
 }
 
@@ -174,6 +259,31 @@ const weather: Weather = {
   &__icon {
     width: em(50);
     height: em(50);
+  }
+}
+
+.swiper {
+  &__item {
+    padding: em(12) em(12) em(28);
+    background: rgba(255, 255, 255, 0.05);
+    border-radius: em(12);
+    border: em(1) solid rgba(#9c8cff, 0.36);
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  &__img {
+    border-radius: em(10);
+    width: 100%;
+    height: em(250);
+    margin-bottom: em(20);
+  }
+
+  &__icon {
+    width: em(20);
+    height: em(20);
   }
 }
 </style>
